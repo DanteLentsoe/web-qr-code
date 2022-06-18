d36a3cc58de1990948ea9accfcb3ae0bcd925ae1
dantelentsoe <dllentsoe@gmail.com>
2022-06-18 21:00:59 +0200

    chore: add types QR code data

---
+++ contants/types/index.ts
@@ -0,0 +1,13 @@
+export interface IQRData {
+  text: string;
+  rawBytes: Array<any>;
+  numBits: number;
+  format: number;
+  buffer: Array<any>;
+  byteLength: number;
+  byteOffset: number;
+  length: number;
+  resultMetadata: Array<any>;
+  resultPoints: Array<any>;
+  timestamp: number;
+}
