9197da48adc51c89390a216380d352b9972c9c5d
dantelentsoe <dllentsoe@gmail.com>
2022-06-19 21:56:53 +0200

    refactor: update text

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -5,42 +5,55 @@ import {
   Stack,
   Text,
   Link,
+  useMediaQuery,
 } from "@chakra-ui/react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { BiDonateHeart } from "react-icons/bi";
 
-const Footer = () => (
-  <>
-    <hr />
-    <Container as="footer" role="contentinfo" py={{ base: "2", md: "2" }}>
-      <Stack spacing={{ base: "-10", md: "-10" }}>
-        <Stack justify="space-between" direction="column" align="flex-end">
-          <ButtonGroup variant="ghost">
-            <IconButton
-              as="a"
-              href="https://www.linkedin.com/in/dante-lentsoe-2b6566148/"
-              aria-label="LinkedIn"
-              icon={<FaLinkedin fontSize="1.25rem" />}
-            />
-            <IconButton
-              as="a"
-              href="https://github.com/dantelentsoe"
-              aria-label="GitHub"
-              icon={<FaGithub fontSize="1.25rem" />}
-            />
-          </ButtonGroup>
+const Footer = () => {
+  const [isMobile] = useMediaQuery("(max-width: 1280px)");
+  return (
+    <>
+      <hr />
+      <Container as="footer" role="contentinfo" py={{ base: "2", md: "2" }}>
+        <Stack spacing={{ base: isMobile ? "0" : "-10", md: "-10" }}>
+          <Stack justify="space-between" direction="column" align="flex-end">
+            <ButtonGroup variant="ghost">
+              <IconButton
+                as="a"
+                href="https://www.linkedin.com/in/dante-lentsoe-2b6566148/"
+                aria-label="LinkedIn"
+                icon={<FaLinkedin fontSize="1.25rem" />}
+              />
+              <IconButton
+                as="a"
+                href="https://github.com/dantelentsoe"
+                aria-label="GitHub"
+                icon={<FaGithub fontSize="1.25rem" />}
+              />
+
+              <IconButton
+                as="a"
+                href="https://www.buymeacoffee.com/dantelentsoe "
+                aria-label="BuyMeACoffe"
+                icon={<BiDonateHeart fontSize="1.25rem" />}
+              />
+            </ButtonGroup>
+          </Stack>
+
+          <Text fontSize="sm" color="subtle" py={{ base: "2", md: "2" }}>
+            &copy; {new Date().getFullYear()} Developed by{" "}
+            <Link
+              href="https://dantelentsoe.com"
+              target="_blank"
+              color={"teal.500"}>
+              Dante Lentsoe
+            </Link>
+          </Text>
         </Stack>
-        <Text fontSize="sm" color="subtle" py={{ base: "2", md: "2" }}>
-          &copy; {new Date().getFullYear()} Developed by{" "}
-          <Link
-            href="https://dantelentsoe.com"
-            target="_blank"
-            color={"teal.500"}>
-            Dante Lentsoe
-          </Link>
-        </Text>
-      </Stack>
-    </Container>
-  </>
-);
+      </Container>
+    </>
+  );
+};
 
 export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,49 +10,52 @@ import HomePage from "../assets/SVG/homePage";
 import { useRouter } from "next/router";
 import Footer from "../components/footer";
 
-export default function CallToActionWithIllustration() {
+export default function Home() {
   const router = useRouter();
+
   return (
-    <Container maxW={"5xl"}>
-      <Stack
-        textAlign={"center"}
-        align={"center"}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}>
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
-          lineHeight={"110%"}>
-          Web QR Code Scanner{" "}
-          <Text as={"span"} color={"teal.600"}>
-            scanning made easy
+    <>
+      <Container maxW={"5xl"}>
+        <Stack
+          textAlign={"center"}
+          align={"center"}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 20, md: 28 }}>
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "3xl", sm: "4xl", md: "6xl" }}
+            lineHeight={"110%"}>
+            Web QR Code Scanner{" "}
+            <Text as={"span"} color={"teal.600"}>
+              scanning made easy
+            </Text>
+          </Heading>
+          <Text color={"gray.500"} maxW={"3xl"}>
+            Scan QR codes, using this easy to use web application, to simplify
+            the scanning process
           </Text>
-        </Heading>
-        <Text color={"gray.500"} maxW={"3xl"}>
-          Scan QR codes, using this easy to use web application, to simplify the
-          scanning process
-        </Text>
-        <Stack spacing={6} direction={"row"}>
-          <Button
-            rounded={"full"}
-            px={6}
-            colorScheme={"teal"}
-            bg={"teal.600"}
-            onClick={() => {
-              router.push("/barcode");
-            }}
-            _hover={{ bg: "teal.500" }}>
-            Get started
-          </Button>
-          <Button rounded={"full"} px={6}>
-            Learn more
-          </Button>
+          <Stack spacing={6} direction={"row"}>
+            <Button
+              rounded={"full"}
+              px={6}
+              colorScheme={"teal"}
+              bg={"teal.600"}
+              onClick={() => {
+                router.push("/barcode");
+              }}
+              _hover={{ bg: "teal.500" }}>
+              Get started
+            </Button>
+            <Button rounded={"full"} px={6}>
+              Learn more
+            </Button>
+          </Stack>
+          <Flex w={"full"} style={{ justifyContent: "center" }}>
+            <HomePage />
+          </Flex>
         </Stack>
-        <Flex w={"full"} style={{ justifyContent: "center" }}>
-          <HomePage />
-        </Flex>
-      </Stack>
-      <Footer />
-    </Container>
+        <Footer />
+      </Container>
+    </>
   );
 }
