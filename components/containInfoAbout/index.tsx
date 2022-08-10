6e6385653cc31d2e04dc619d20532ab8dc7eff96
dantelentsoe <dllentsoe@gmail.com>
2022-08-10 20:25:58 +0200

    chore: grammer fix

--- components/containInfoAbout/index.tsx
+++ components/containInfoAbout/index.tsx
@@ -27,7 +27,7 @@ const ContentQRCode = () => {
                 bg: "teal.400",
                 zIndex: -1,
               }}>
-              How Does it Works
+              How Does it Work
             </Text>
             <br />
             <Text
